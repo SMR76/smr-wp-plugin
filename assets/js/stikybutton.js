044a63de86158840b57dfb1b40c723107e572cd8
SMR <seyyedmortezarazavi76@gmail.com>
2021-07-25 17:07:44 +0430

    renamed some class id and files

--- assets/js/stikybutton.js
+++ assets/js/stikybutton.js
@@ -1,6 +1,6 @@
 
 function toggle(){
-    let options = document.getElementsByClassName('option');
+    let options = document.getElementsByClassName('smr-stikybutton-option');
     for(let x of options)
         x.classList.toggle("active");
 }
