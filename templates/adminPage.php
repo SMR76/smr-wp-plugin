27897fdf8d82092d168070408e8eaffced7ef12c
SMR <seyyedmortezarazavi76@gmail.com>
2021-07-28 02:07:29 +0430

    moved autocompletor to singular file.

--- templates/adminPage.php
+++ templates/adminPage.php
@@ -8,6 +8,8 @@
 <div class="wrap">
     <h3>General Page</h3>
     <?php
+        wp_enqueue_script("simple-autocomplete",$this->pluginUrl.'/assets/js/simple-autocomplete.js',['jquery']);
+        
         global $wp_roles;
         
         settings_errors();         
@@ -21,47 +23,11 @@
             submit_button();
         ?>
     </form>
-
-    
+        
     <script>
-        jQuery(function($){
-            jQuery('input#qty_args').click(function(){
-                if( jQuery(this).is(':checked')) {
-                    jQuery('div.qty-args').removeClass('hidden');
-                } else {
-                    jQuery('div.qty-args').addClass('hidden');
-                }
-            });
-
-            var source=["<?php echo implode('","',array_keys($allRoleNames)) ?>"];
-            
-            var suggestionClicked = function() {
-                let suggest = jQuery(this).html();
-                let input   = jQuery('#ws-selected-roles');
-                let value   = input.val();
-                let lastIndex   = value.lastIndexOf(',');
-                let newInput    = lastIndex == -1? '' : value.substr(0,lastIndex) + ',';
-
-                input.val(newInput + suggest);
-                jQuery("#suggestionListContainer").html('');
-            };
-
-            jQuery('#ws-selected-roles').keyup(function() {
-                let rolesInput  = jQuery(this).val().toLocaleLowerCase().replaceAll(' ','');
-                let lastIndex   = rolesInput.lastIndexOf(',');
-                let result      = []; 
-
-                rolesInput = lastIndex == -1 ? rolesInput : rolesInput.substr(lastIndex + 1);
-                for(let x of source) {
-                    if(rolesInput != '' && x.toLocaleLowerCase().includes(rolesInput) == true) {
-                        result.push(`<a class="btn suggestedItem">${x}</a>`);
-                    }
-                }
-                jQuery("#suggestionListContainer").html(result.join(', '));
-
-                if(result.length)
-                    jQuery('.suggestedItem').click(suggestionClicked);
-            });
+        jQuery(document).ready(() => {
+            let source = ["<?php echo implode('","',array_keys($allRoleNames)) ?>"];
+            var autoComplete = new autoCompleter(source,"#suggestionListContainer","#ws-selected-roles");
         });
     </script>
 </div>
