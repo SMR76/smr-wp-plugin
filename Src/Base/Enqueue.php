c40f9b9035f72a3806020ae272213be0aaadee89
SMR <seyyedmortezarazavi76@gmail.com>
2021-08-03 03:09:09 +0430

    removed enqueue lottie-player from Enqueue class.

--- Src/Base/Enqueue.php
+++ Src/Base/Enqueue.php
@@ -9,11 +9,11 @@ use \Src\Base\BaseController;
 
 class Enqueue extends BaseController {
     public  function register() {
-        wp_register_script('lottiePlayer',"$this->pluginUrl/assets/js/lottie-player.js");
-        add_action( 'wp_footer', array($this, 'enqueue'));
+        // wp_register_script('lottiePlayer',"$this->pluginUrl/assets/js/lottie-player.js");
+        // add_action( 'wp_footer', array($this, 'enqueue'));
     }	
     
     public function enqueue() {
-        wp_enqueue_script('lottiePlayer');
+        // wp_enqueue_script('lottiePlayer');
 	}
 }
