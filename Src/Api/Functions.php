c3a1ab4f447bb6e5386a40b280803c03327cfef6
SMR <seyyedmortezarazavi76@gmail.com>
2021-08-07 12:20:52 +0430

    added new sections. added new fields.

--- Src/Api/Functions.php
+++ Src/Api/Functions.php
@@ -8,19 +8,51 @@ namespace Src\Api;
 use \Src\Base\BaseController;
 
 class Functions extends BaseController{
+    /**
+     * admin setting page (general page)
+     */
     public function adminGeneralPage() {
         return require_once("$this->pluginPath/templates/adminPage.php"); 
     }
-    public function optionGroupFieldsFilter($input) {
+
+    /**
+     * sanetize group
+     */
+    public function optionGroupFieldsFilter($input) {  
         return $input;
     }
-    public function adminSectionArea() {
-        _e('<h5>Roles</h5>','smr-plugin');
+
+    public function wholesaleSection() {
+        _e('set default valid roles of wholesale users.','smr-plugin');
     }
+
+    public function activateOptionsSection() {
+        _e('activate or deactivate other parts of plugin.','smr-plugin');
+    }
+
+    //* ---------------------- fields -----------------------------
     public function wholesaleRolesInput() {
-        $value = esc_attr(get_option('smr_wholesale'));
-        printf("<input id='ws-selected-roles' type='text' class='form-control' 
-                name='smr_wholesale' value='%s' placeholder='' style='width: 400px;direction: ltr;' pattern='^\w+(\s*,\s*\w+)*$'>", $value);
-        echo '<p id="suggestionListContainer" class="form-field" style="height:20px;"></p>';
+        $values     = get_option('smr_settings_option_group');
+        $wsRoles    = isset($values['ws_roles']) ? $values['ws_roles'] : '';
+        ?>
+        <input id='ws-selected-roles' type='text' name='smr_settings_option_group[ws_roles]'
+                style='width:400px;direction:ltr;' pattern='^\w+(\s*,\s*\w+)*$'
+                placeholder = "e.g. wholesale, costomer etc." value='<?php echo $wsRoles;?>'>				
+        <p id="suggestionListContainer" class="form-field" style="height:20px;"></p>
+        <?php
+    }
+
+    public function activateWholesale() {
+        $values     = get_option('smr_settings_option_group');
+        ?>
+        <input id="activate_wholesale" type='checkbox' name='smr_settings_option_group[activate_wholesale]' <?php echo isset($values['activate_wholesale'])? 'checked' : '';?>> 
+        <?php
+    }
+
+    public function activateCheckout() {
+        $values     = get_option('smr_settings_option_group');
+        ?>
+        <input type='checkbox' name='smr_settings_option_group[activate_checkout]' <?php echo isset($values['activate_checkout'])? 'checked' : '';?>> 
+        <?php
     }
 }
